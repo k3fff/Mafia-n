c0460063d69eece6588bd327429ba257287d33fc
k3fff <154376178+k3fff@users.noreply.github.com>
2023-12-20 23:06:46 +0200

    Update unit.js

--- unit.js
+++ unit.js
@@ -5,11 +5,16 @@ const factorial = require('./factorial');
 function test(description, actual, expected) {
   try {
     assert.strictEqual(actual, expected);
-    console.log(`Перевірка пройшла успішно: ${description}`);
+    console.log(`✅ Перевірка пройшла успішно: ${description}`);
   } catch (error) {
-    console.error(` Перевірка не пройшла: ${description}`);
+    console.error(`❌ Перевірка не пройшла: ${description}`);
     console.error(`   Очікувалось: ${expected}`);
     console.error(`   Отримано: ${actual}`);
   } 
 }
+
+// Тести
+test('Факторіал 0 має бути 1', factorial(0), 1);
+test('Факторіал 1 має бути 1', factorial(1), 1);
+test('Факторіал 5 має бути 120', factorial(5), 120);
 test('Факторіал 10 має бути 3628800', factorial(10), 3628800);
